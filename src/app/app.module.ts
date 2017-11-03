32b96e48e63d7eed2b90f9c207f7c20ea5a43791
Fred Krauweel <f.krauweel@gmail.com>
2017-11-03 19:37:13 +0100

    klaar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GroenComponent } from './groen/groen.component';
 
@@ -10,7 +10,8 @@ import { GroenComponent } from './groen/groen.component';
     GroenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/groen/groen.component.ts
+++ src/app/groen/groen.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groen.component.css']
 })
 export class GroenComponent implements OnInit {
+showModal = false;
+rate: string = '';
+name: string = '';
+email: string = ''; 
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+  }
+
+  openModal(){
+    this.rate = '';
+    this.name = '';
+    this.email = ''; 
+    this.showModal = true;
+  }
+  closeModal(){
+    this.showModal = false;
+  }
+  sendData(){
+    console.log("rate: " + this.rate + "\nname: " + this.name + "\nemail: " + this.email);
+    this.closeModal();
   }
 
 }
